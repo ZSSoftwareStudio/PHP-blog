0cadd4c82d2cf5e14c77c147f6ac69ce3eb2e658
Zarifprogrammer <Zarifprogrammer@gmail.com>
2020-07-01 22:52:17 +0600

    fourth commit

---
+++ fcategories.php
@@ -0,0 +1,22 @@
+<?php
+    include("Components/_conn.php");
+    $sql = "select * from `categories`";
+    $query = mysqli_query($conn, $sql);
+    while($row = mysqli_fetch_assoc($query)){
+        echo '
+        <div class="col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <img src="' . $row['url'] . '" class="card-img-top mx-auto" alt="' . $row['name'] . '" style="width: 120px; height: 120px;">
+                <div class="card-body">
+                    <h5 class="card-title">' . $row['name'] . '</h5>
+                    <p class="card-text">' . substr($row['description'], 0, 110) . '...</p>
+                    <a href="#" class="btn btn-primary">View Category</a>
+                </div>
+            </div>
+        </div>
+        ';
+    }
+
+?>
+
+
--- index.php
+++ index.php
@@ -1,27 +1,30 @@
 <?php include("Components/_header.php"); ?>
 <div>
-<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/0.jpg" class="d-block w-100" alt="...">
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/0.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/2.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/3.jpg" class="d-block w-100" alt="...">
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="img/2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/3.jpg" class="d-block w-100" alt="...">
-    </div>
+    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-</div> 
-<?php include("fcategories.php")?>
+<div class="container my-4">
+  <div class="row">
+    <?php include("fcategories.php")?>
+  </div>
+</div>
 <?php include("Components/_footer.php"); ?>
-
