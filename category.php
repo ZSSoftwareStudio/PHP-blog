80252882f88a69038ac87b24e3cdcde1d0ed3a5b
Zarifprogrammer <Zarifprogrammer@gmail.com>
2020-07-12 22:12:46 +0600

    eleventh commit

--- category.php
+++ category.php
@@ -1,7 +1,25 @@
+<?php include("Components/_header.php"); ?>
 <?php
+    include("Components/_conn.php");
+    $category = $_GET['category'];
 
-    $category_name = $_GET['category'];
-    echo $category_name;
+    $sql = "SELECT * FROM `categories` WHERE `name` = '". $category ."' LIMIT 1";
+    $query = mysqli_query($conn, $sql);
 
-
+    $row = mysqli_fetch_assoc($query);
+    
+    echo '<div class="jumbotron jumbotron-fluid">
+            <div class="container">
+            <h1 class="display-4 text-center">'.$row['name'].'</h1>
+            <br/>
+            <p class="lead container">'. $row['description'] .'</p>
+            </div>
+        </div>';
 ?>
+  
+<div class="container my-4">
+  <div class="row">
+
+  </div>
+</div>
+<?php include("Components/_footer.php"); ?>
