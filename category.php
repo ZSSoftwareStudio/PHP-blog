ce995a1dd630a58ef242051b50f10a629fe67644
Zarifprogrammer <Zarifprogrammer@gmail.com>
2020-07-14 23:57:45 +0600

    ...

--- category.php
+++ category.php
@@ -19,7 +19,25 @@
   
 <div class="container my-4">
   <div class="row">
+      <?php
+        $sql2 = "SELECT * FROM `blogs` WHERE `cat` = '". $category ."'";
+        $query2 = mysqli_query($conn, $sql2);
 
+        while ($row2 = mysqli_fetch_assoc($query2)){
+          echo '
+          <div class="card" style="width: 18rem;">
+            <div class="card-header">
+              '. $row2['cat'] .'
+            </div>
+            <div class="card-body" style="width: 18rem;">
+              <h5 class="card-title">'. $row2['name'] .'</h5>
+              <p class="card-text">'. substr($row2['description'], 0, 100) .'... </p>
+              <a href="#" class="btn btn-primary">Read More</a>
+            </div>
+          </div>
+          ';
+        }
+      ?>
   </div>
 </div>
 <?php include("Components/_footer.php"); ?>
